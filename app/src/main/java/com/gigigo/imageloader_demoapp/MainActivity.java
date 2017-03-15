ce53878a22ef388f56b1400b9e69bc7c2607efe5
Pablo Rojas <pablo.rojas@gigigo.com>
2017-03-15 10:57:48 +0100

    Added dialog to show image

--- app/src/main/java/com/gigigo/imageloader_demoapp/MainActivity.java
+++ app/src/main/java/com/gigigo/imageloader_demoapp/MainActivity.java
@@ -1,14 +1,17 @@
 package com.gigigo.imageloader_demoapp;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
-import android.os.Bundle;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import com.bumptech.glide.Glide;
 import com.gigigo.ui.imageloader.ImageLoader;
 import com.gigigo.ui.imageloader.ImageLoaderCallback;
 import com.gigigo.ui.imageloader.glide.GlideImageLoaderImp;
@@ -17,88 +20,90 @@ import com.gigigo.ui.imageloader.picasso.PicassoImageLoaderImp;
 
 public class MainActivity extends AppCompatActivity {
 
+  private Button btnGlideInto;
+  private Button btnGlideCallback;
+  private Button btnGlideError;
+  private Button btnPicassoInto;
+  private Button btnPicassoCallback;
+  private Button btnPicassoError;
+  private Button btnGlideResource;
+  private Button btnPicassoResource;
+  private Button btnGlideCenterCrop;
+  private Button btnPicassoCenterCrop;
+  private Button btnGlideOverride;
+  private Button btnPicassoOverride;
+
   private ImageLoader imageLoader;
-
   private ImageView imageView;
-  private Button buttonGlide;
-  private Button buttonPicasso;
-
-
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    setContentView(R.layout.activity_menu);
 
+    btnGlideCallback = (Button) findViewById(R.id.button_glide_callback);
+    btnGlideInto = (Button) findViewById(R.id.button_glide_into);
+    btnGlideError = (Button) findViewById(R.id.button_glide_error);
+    btnGlideResource = (Button) findViewById(R.id.button_glide_resource);
+    btnGlideCenterCrop = (Button) findViewById(R.id.button_glide_center_crop);
+    btnGlideOverride = (Button) findViewById(R.id.button_glide_override);
 
+    btnPicassoCallback = (Button) findViewById(R.id.button_picasso_callback);
+    btnPicassoInto = (Button) findViewById(R.id.button_picasso_into);
+    btnPicassoError = (Button) findViewById(R.id.button_picasso_error);
+    btnPicassoResource = (Button) findViewById(R.id.button_picasso_resource);
+    btnPicassoCenterCrop = (Button) findViewById(R.id.button_picasso_center_crop);
+    btnPicassoOverride = (Button) findViewById(R.id.button_picasso_override);
 
-    initView();
+    LayoutInflater inflater = getLayoutInflater();
+    View view = inflater.inflate(R.layout.dialog_image, null);
+    imageView = (ImageView) view.findViewById(R.id.imageview2);
 
+    final Dialog dialog = onCreateDialog(view);
 
-
-  }
-
-  private void initView() {
-
-    imageView = (ImageView) findViewById(R.id.imageview);
-    buttonGlide = (Button) findViewById(R.id.button_glide);
-    buttonPicasso = (Button) findViewById(R.id.button_picasso);
-
-
-
-    buttonGlide.setOnClickListener(new View.OnClickListener() {
+    btnGlideInto.setOnClickListener(new View.OnClickListener() {
       @Override public void onClick(View v) {
         setGlideImageLoader();
+
         String url = DataGenerator.generateRandomImageUrl();
 
-        Snackbar.make(buttonGlide, url, Snackbar.LENGTH_SHORT).show();
+        Snackbar.make(btnGlideInto, url, Snackbar.LENGTH_SHORT).show();
 
         //TODO: Download more transformations: https://github.com/wasabeef/glide-transformations
         imageLoader.load(url)
             .placeholder(R.drawable.ic_loading)
-            .error(R.drawable.ic_loading)
-            .transform(new GlideCircleTransformation(MainActivity.this, 12, getResources().getColor(android.R.color.black)))
-            //.transform(new RoundedCornersTransformation(MainActivity.this, 20, 20))
+            .error(R.drawable.errorimage)
             .into(imageView)
-            //.loaderCallback(new ImageLoaderCallback() {
-            //  @Override public void onSuccess(Bitmap bitmap) {
-            //    Snackbar.make(buttonGlide, "image Glide loaded!", Snackbar.LENGTH_SHORT).show();
-            //    imageView.setImageBitmap(bitmap);
-            //  }
-            //
-            //  @Override public void onError(Drawable errorDrawable) {
-            //    Snackbar.make(buttonGlide, "image Glide error :(", Snackbar.LENGTH_SHORT).show();
-            //    imageView.setImageResource(R.drawable.ic_loading);
-            //  }
-            //
-            //  @Override public void onLoading() {
-            //    imageView.setImageResource(R.drawable.ic_loading);
-            //  }
-            //})
             .build();
+        dialog.show();
       }
     });
 
-    buttonPicasso.setOnClickListener(new View.OnClickListener() {
+    btnGlideCallback.setOnClickListener(new View.OnClickListener() {
       @Override public void onClick(View v) {
-        setPicassoImageLoader();
-        String url = DataGenerator.generateRandomImageUrl();
-        Snackbar.make(buttonPicasso, url, Snackbar.LENGTH_SHORT).show();
 
-        //TODO: test it with circleimage
+        setGlideImageLoader();
+        String url = DataGenerator.generateRandomImageUrl();
+
+        Snackbar.make(btnGlideCallback, url, Snackbar.LENGTH_SHORT).show();
+
+        //TODO: Download more transformations: https://github.com/wasabeef/glide-transformations
         imageLoader.load(url)
-            .placeholder(R.drawable.ic_loading)
-            .error(R.drawable.ic_loading)
-            //.into(imageView)
+            .transform(new GlideCircleTransformation(MainActivity.this, 12,
+                getResources().getColor(android.R.color.black)))
+            //.transform(new RoundedCornersTransformation(MainActivity.this, 20, 20))
+
             .loaderCallback(new ImageLoaderCallback() {
               @Override public void onSuccess(Bitmap bitmap) {
-                Snackbar.make(buttonGlide, "image Picasso loaded!", Snackbar.LENGTH_SHORT).show();
+                Snackbar.make(btnGlideCallback, "image Glide loaded!", Snackbar.LENGTH_SHORT)
+                    .show();
                 imageView.setImageBitmap(bitmap);
+                dialog.show();
               }
 
               @Override public void onError(Drawable errorDrawable) {
-                Snackbar.make(buttonGlide, "image Picasso error :(", Snackbar.LENGTH_SHORT)
+                Snackbar.make(btnGlideCallback, "image Glide error :(", Snackbar.LENGTH_SHORT)
                     .show();
-                imageView.setImageResource(R.drawable.ic_loading);
+                imageView.setImageResource(R.drawable.errorimage);
               }
 
               @Override public void onLoading() {
@@ -109,8 +114,207 @@ public class MainActivity extends AppCompatActivity {
       }
     });
 
+    btnGlideResource.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View v) {
+        setGlideImageLoader();
 
+        Snackbar.make(btnGlideCallback, "Image loaded from resource", Snackbar.LENGTH_SHORT).show();
+
+        imageLoader.load(R.drawable.resourceimage)
+            .placeholder(R.drawable.ic_loading)
+            .error(R.drawable.errorimage)
+            .into(imageView)
+            .build();
+        dialog.show();
+      }
+    });
+
+    btnGlideCenterCrop.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View v) {
+
+        setGlideImageLoader();
+        String url = DataGenerator.generateRandomImageUrl();
+
+        Snackbar.make(btnGlideCenterCrop, url, Snackbar.LENGTH_SHORT).show();
+
+        //TODO: Download more transformations: https://github.com/wasabeef/glide-transformations
+        imageLoader.load(url)
+            .placeholder(R.drawable.ic_loading)
+            .error(R.drawable.errorimage)
+
+            //.transform(new GlideCircleTransformation(MainActivity.this, 12,
+            //    getResources().getColor(android.R.color.black)))
+            //.transform(new RoundedCornersTransformation(MainActivity.this, 20, 20))
+            .into(imageView)
+            .centerCrop(Boolean.TRUE)
+            .build();
+        dialog.show();
+      }
+    });
+
+    btnGlideOverride.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View v) {
+        setGlideImageLoader();
+        String url = DataGenerator.generateRandomImageUrl();
+
+        Snackbar.make(btnGlideOverride, url, Snackbar.LENGTH_SHORT).show();
+        imageLoader.load(url)
+            .placeholder(R.drawable.ic_loading)
+            .error(R.drawable.errorimage)
+            .override(50,50)
+            .into(imageView)
+            .build();
+        dialog.show();
+      }
+    });
+
+    btnGlideError.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View v) {
+        setGlideImageLoader();
+        String url = "Error";
+
+        Snackbar.make(btnGlideCallback, url, Snackbar.LENGTH_SHORT).show();
+
+        imageLoader.load(url)
+            .placeholder(R.drawable.ic_loading)
+            .error(R.drawable.errorimage)
+            .transform(new GlideCircleTransformation(MainActivity.this, 12,
+                getResources().getColor(android.R.color.black)))
+            .into(imageView)
+            .build();
+        dialog.show();
+      }
+    });
+
+    btnPicassoInto.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View v) {
+
+        setPicassoImageLoader();
+        String url = DataGenerator.generateRandomImageUrl();
+        Snackbar.make(btnPicassoInto, url, Snackbar.LENGTH_SHORT).show();
+
+        //TODO: test it with circleimage
+        imageLoader.load(url)
+            .placeholder(R.drawable.ic_loading)
+            .error(R.drawable.errorimage)
+            .into(imageView)
+            .build();
+        dialog.show();
+      }
+    });
+
+    btnPicassoCallback.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View v) {
+
+        setPicassoImageLoader();
+        String url = DataGenerator.generateRandomImageUrl();
+        Snackbar.make(btnPicassoCallback, url, Snackbar.LENGTH_SHORT).show();
+
+        //TODO: test it with circleimage
+        imageLoader.load(url).loaderCallback(new ImageLoaderCallback() {
+          @Override public void onSuccess(Bitmap bitmap) {
+            Snackbar.make(btnPicassoCallback, "image Picasso loaded!", Snackbar.LENGTH_SHORT)
+                .show();
+            imageView.setImageBitmap(bitmap);
+          }
+
+          @Override public void onError(Drawable errorDrawable) {
+            Snackbar.make(btnPicassoCallback, "image Picasso error :(", Snackbar.LENGTH_SHORT)
+                .show();
+            imageView.setImageResource(R.drawable.errorimage);
+          }
+
+          @Override public void onLoading() {
+            imageView.setImageResource(R.drawable.ic_loading);
+          }
+        }).build();
+        dialog.show();
+      }
+    });
+
+    btnPicassoResource.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View v) {
+
+        setPicassoImageLoader();
+
+        imageLoader.load(R.drawable.resourceimage)
+            .placeholder(R.drawable.ic_loading)
+            .error(R.drawable.errorimage)
+            .into(imageView)
+            .build();
+        dialog.show();
+      }
+    });
+
+    btnPicassoCenterCrop.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View v) {
+
+        setPicassoImageLoader();
+        String url = DataGenerator.generateRandomImageUrl();
+        Snackbar.make(btnPicassoInto, url, Snackbar.LENGTH_SHORT).show();
+
+        //TODO: test it with circleimage
+        imageLoader.load(url)
+            .placeholder(R.drawable.ic_loading)
+            .error(R.drawable.errorimage)
+            .into(imageView)
+            .centerCrop(Boolean.TRUE)
+            .override(200, 200)
+            .build();
+        dialog.show();
+      }
+    });
+
+    btnPicassoOverride.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View v) {
+        setPicassoImageLoader();
+        String url = DataGenerator.generateRandomImageUrl();
+
+        Snackbar.make(btnPicassoOverride, url, Snackbar.LENGTH_SHORT).show();
+        imageLoader.load(url)
+            .placeholder(R.drawable.ic_loading)
+            .error(R.drawable.errorimage)
+            .override(50,50)
+            .into(imageView)
+            .build();
+        dialog.show();
+      }
+    });
+
+
+    btnPicassoError.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View v) {
+
+        setPicassoImageLoader();
+        String url = "Error";
+        Snackbar.make(btnPicassoCallback, url, Snackbar.LENGTH_SHORT).show();
+
+        imageLoader.load(url)
+            .placeholder(R.drawable.ic_loading)
+            .error(R.drawable.errorimage)
+            .into(imageView)
+            .build();
+        dialog.show();
+      }
+    });
   }
+
+  public Dialog onCreateDialog(View view) {
+    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+    // Get the layout inflater
+
+    // Inflate and set the layout for the dialog
+    // Pass null as the parent view because its going in the dialog layout
+    builder.setView(view)
+        // Add action buttons
+        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+          public void onClick(DialogInterface dialog, int id) {
+
+          }
+        });
+    return builder.create();
+  }
+
 
   private void setGlideImageLoader() {
     imageLoader = new GlideImageLoaderImp(this);

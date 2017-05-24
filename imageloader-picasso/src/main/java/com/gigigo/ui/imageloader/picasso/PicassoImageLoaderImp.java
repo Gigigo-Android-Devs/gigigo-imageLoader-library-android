51935b7f5532f8de61bd2004983de421d4207d23
Pablo Rojas <pablo.rojas@gigigo.com>
2017-05-24 12:30:59 +0200

    Pause - Resume Picasso

--- imageloader-picasso/src/main/java/com/gigigo/ui/imageloader/picasso/PicassoImageLoaderImp.java
+++ imageloader-picasso/src/main/java/com/gigigo/ui/imageloader/picasso/PicassoImageLoaderImp.java
@@ -3,9 +3,10 @@ package com.gigigo.ui.imageloader.picasso;
 import android.content.Context;
 import com.gigigo.ui.imageloader.ImageLoader;
 import com.gigigo.ui.imageloader.ImageLoaderBuilder;
+import com.squareup.picasso.Picasso;
 
 public class PicassoImageLoaderImp extends ImageLoaderBuilderImp implements ImageLoader {
-
+  Context context;
 
   public PicassoImageLoaderImp(Context context) {
     super(context);
@@ -24,10 +25,12 @@ public class PicassoImageLoaderImp extends ImageLoaderBuilderImp implements Imag
   }
 
   @Override public ImageLoaderBuilder pauseRequests() {
-    return null;
+    Picasso.with(context).pauseTag(context);
+    return this;
   }
 
   @Override public ImageLoaderBuilder resumeRequests() {
+    Picasso.with(context).resumeTag(context);
     return null;
   }
 }
